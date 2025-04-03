92c6194962e9f95cbb3fe65c5627ff8bfa39966f
Aynur Karimov <hey@aynurkarimov.com>
2025-04-03 13:04:14 +0400

    Count height of a tree + depth of a node

---
+++ data_structures/binaryTree_count.js
@@ -0,0 +1,70 @@
+const tree = {
+  value: 20,
+  left: {
+    value: 10,
+    left: {
+      value: 5,
+      left: null,
+      right: {
+        value: 7,
+        left: null,
+        right: null,
+      },
+    },
+    right: {
+      value: 15,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    value: 50,
+    left: {
+      value: 30,
+      left: {
+        value: 29,
+        left: null,
+        right: null,
+      },
+      right: {
+        value: 45,
+        left: null,
+        right: null,
+      },
+    },
+    right: {
+      value: 100,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+function treeHeight(treeNode) {
+  if (!treeNode) {
+    return -1;
+  }
+
+  const l = treeHeight(treeNode.left);
+  const r = treeHeight(treeNode.right);
+
+  return Math.max(l, r) + 1;
+}
+
+function treeDepth(treeNode, target, depth = 0) {
+  if (!treeNode) {
+    return -1;
+  }
+
+  if (treeNode.value === target) {
+    return depth;
+  }
+
+  const l = treeDepth(treeNode.left, target, depth + 1);
+  const r = treeDepth(treeNode.right, target, depth + 1);
+
+  return Math.max(l, r);
+}
+
+console.log("height is", treeHeight(tree));
+console.log("depth of value 30 is", treeDepth(tree, 30));
